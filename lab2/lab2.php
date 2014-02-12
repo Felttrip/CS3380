a9916122e8b6f16eb05deffcf76b4d88b5cc0be4
Nathaniel Thompson <nthompson55@gmail.com>
2014-02-12 10:26:08 -0600

    querys up to 9 work and formatting has been changedto meet lab requirements

--- lab2/lab2.php
+++ lab2/lab2.php
@@ -3,48 +3,47 @@
   <head>
     <meta charset=UTF-8>
     <title>Nathaniel Thompson Lab 2</title>
+  </head>
 
     <body>
       <form method = "POST" action = "<?= $_SERVER['PHP_SELF'] ?>">
       <select name = "query">
         <option value="1" >Query 1</option>
-	<option value="2" >Query 2</option>
-	<option value="3" >Query 3</option>
-	<option value="4" >Query 4</option>
-	<option value="5" >Query 5</option>
-	<option value="6" >Query 6</option>
-	<option value="7" >Query 7</option>
-	<option value="8" >Query 8</option>
-	<option value="9" >Query 9</option>
-	<option value="10" >Query 10</option>
-	<option value="11" >Query 11</option>
-	<option value="12" >Query 12</option>
+        <option value="2" >Query 2</option>
+        <option value="3" >Query 3</option>
+        <option value="4" >Query 4</option>
+        <option value="5" >Query 5</option>
+        <option value="6" >Query 6</option>
+        <option value="7" >Query 7</option>
+        <option value="8" >Query 8</option>
+        <option value="9" >Query 9</option>
+        <option value="10" >Query 10</option>
+        <option value="11" >Query 11</option>
+        <option value="12" >Query 12</option>
       </select>
      <input type="submit" name="submit" value="Execute"/>
     </form>
-    </body>
-  </head>
-</html>
+    
 <?php
   //set up database
   include("../secure/database.php");
   $conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect:' . pg_last_error());
   
-  $value = $_POST['query']
+  $value = $_POST['query'];
+  $query = null;
   switch ($value)
   {
     case 1:
-      $query = 'SELECT name,region,continent,government_form 
+      $query = 'SELECT name ,region,continent,government_form 
                 FROM lab2.country 
                 WHERE surface_area > 2000000 
-                ORDER BY name';
-      echo "Countrys with a surface area over 2000000";
+                ORDER BY gnp DESC';
       break;
     case 2:
-      $query = 'SELECT name, language 
+      $query = 'SELECT name, language, cl.percentage
                 FROM lab2.country AS co, lab2.country_language AS cl 
                 WHERE(cl.percentage>50) AND (cl.is_official = false) AND  (co.country_code=cl.country_code) 
-                ORDER BY language';
+                ORDER BY cl.percentage DESC';
       break;
     case 3:
       $query = 'SELECT DISTINCT language 
@@ -53,24 +52,37 @@
                 ORDER BY language';
       break;
     case 4:
-      $query = 'SELECT ci.name, ci.district, co.name as CountryName 
+      $query = 'SELECT ci.name AS city, ci.district, co.name as Country
                 FROM lab2.country AS co, lab2.city as ci 
                 WHERE (co.country_code=ci.country_code) 
                 ORDER BY co.name, ci.population DESC, ci.name';
       break;
     case 5:
-      $query = 'SELECT co.name, ci.name AS capital, language, round(((percentage/100)*co.population)::numeric,0) AS population 
+      $query = 'SELECT co.name, ci.name AS capital, language, round(((percentage/100)*co.population)::numeric,0) AS speakers 
                 FROM lab2.country AS co, lab2.city AS ci, lab2.country_language AS cl 
                 WHERE (co.capital = ci.id) AND (co.country_code=cl.country_code) AND (is_official=true) 
-                ORDER BY co.name, population DESC, capital';
+                ORDER BY co.name, speakers DESC, capital';
       break;
     case 6:
+      $query = 'SELECT name AS city, district, population 
+                FROM  lab2.city
+                WHERE (population > 3500000)
+                ORDER BY name';
       break;
     case 7:
+      $query = 'SELECT ci.name AS City,ci.district,co.name AS Country
+                FROM lab2.city AS ci, lab2.country AS co
+                WHERE ci.name LIKE \'S%s\' AND (ci.country_code = co.country_code)
+                ORDER BY co.name,ci.name';
       break;
     case 8:
+      $query = 'SELECT DISTINCT co.name AS Country
+                FROM lab2.country AS co, lab2.country_language AS cl
+                WHERE (co.population >10000000) AND (cl.is_official=false) AND (co.country_code = cl.country_code) AND (cl.percentage>20)
+                ORDER BY co.name';
       break;
     case 9:
+      $query = 'SELECT ';
       break;
     case 10:
       break;
@@ -81,18 +93,33 @@
     default:
       echo "<strong>Please select a query</strong>";
   }
-  $result = pg_query($query) or die('Query failed' . pg_last_error());
-    
-  //Print results in HTML
-  echo "<table border=\"1\">\n";
-  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+  if($query)
   {
-    foreach($line as $col_value)
+    $result = pg_query($query) or die('Query failed' . pg_last_error());
+    echo "\n<br/>\n<hr/>\n<br/>\n\nThere were <em>".pg_num_rows($result)."</em> rows returned<br/><br/>";
+    //Print results in HTML
+    echo "\n<table border=\"1\">\n\t<tr>\n";
+    while ($i < pg_num_fields($result))
     {
-      echo "\t\t<td>$col_value</td>\n";
+      $fieldName = pg_field_name($result, $i);
+      echo "\t\t<th>" . $fieldName . "</th>\n";
+      $i = $i + 1;
     }
     echo "\t</tr>\n";
+    $i = 0;
+    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+    {
+      echo "\t<tr>\n";
+      foreach($line as $col_value)
+      {
+        echo "\t\t<td>$col_value</td>\n";
+      }
+      echo "\t</tr>\n";
+    }
+    echo "</table>\n";
   }
-  echo "</table>\n";
-  pg_close($conn);
+    pg_close($conn);
 ?>
+    </body>
+  </head>
+</html>
